10242d0539f5ed513be8beb899351963027a26d3
Luis Galeana <lsgaleana@gmail.com>
2025-01-14 22:37:29 -0600

    Add SQLAlchemy model for Opportunity in app/models/Opportunity.py

--- app/models/Opportunity.py
+++ app/models/Opportunity.py
@@ -4,9 +4,7 @@ from app.modassembly.database.sql.get_sql_session import Base
 class Opportunity(Base):
     __tablename__ = 'opportunities'
 
-    type = Column(String, nullable=False, comment="The type of the Salesforce object, e.g., 'Opportunity'.")
-    url = Column(String, nullable=False, comment="The URL to access the Salesforce Opportunity object.")
-    Id = Column(String, primary_key=True, comment="The unique identifier for the Salesforce Opportunity.")
-    GCLID = Column(String, nullable=True, comment="The Google Click Identifier associated with the Opportunity.")
-    CreatedDate = Column(DateTime, nullable=False, comment="The date and time when the Opportunity was created.")
-    Admission_Date = Column(DateTime, nullable=True, comment="The date of admission associated with the Opportunity.")
+    Id = Column(String, primary_key=True, nullable=False, unique=True, doc="The unique identifier for the Salesforce Opportunity.")
+    GCLID__c = Column(String, nullable=True, doc="The Google Click Identifier associated with the Opportunity.")
+    CreatedDate = Column(DateTime, nullable=False, doc="The date and time when the Opportunity was created.")
+    Admission_Date__c = Column(DateTime, nullable=True, doc="The date of admission associated with the Opportunity.")
